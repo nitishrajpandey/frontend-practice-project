b12386e928ed8e960812394c9bcbf4548142fcc1
nitishrajpandey <nitishrajpandey0707@gmail.com>
2024-01-08 20:15:06 +0530

    add project 16

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {},
+    screens: {
+      xxs: "360px",
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    }
   },
   plugins: [],
 }
